0e468990fcd965201391f719055f9f13269f107f
yasainet <takumi.mizoguchi@gmail.com>
2024-11-20 13:57:50 +0900

    修正(metadata.ts): メタデータの説明からカンマを削除

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -2,5 +2,5 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "test.net",
-  description: "test.net, description",
+  description: "test.net description",
 };
