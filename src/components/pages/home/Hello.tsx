3227ccceef1d9ca9d1973cda498f68ab3465319b
yasainet <takumi.mizoguchi@gmail.com>
2024-11-22 01:27:01 +0900

    feat(Hello.tsx): 複数の見出しとテキストコンポーネントを追加 新しいコンポーネントを使用して、より多様なテキスト表現を提供するため。

--- src/components/pages/home/Hello.tsx
+++ src/components/pages/home/Hello.tsx
@@ -1,12 +1,19 @@
-import { H1, H2, Lead, P } from '@/components/ui/typography';
+import { BlockQuote, H1, H2, H3, H4, InlineCode, Large, Lead, Muted, P, Small } from '@/components/ui/typography';
 
 export default function Hello() {
   return (
     <>
-      <H1>hello</H1>
-      <H2>World</H2>
-      <Lead>test.net is test</Lead>
-      <P>this is paragraph.</P>
+      <H1>Hello, World!</H1>
+      <H2>Hello, World!</H2>
+      <H3>Hello, World!</H3>
+      <H4>Hello, World!</H4>
+      <Lead>Hello, World!</Lead>
+      <BlockQuote>Hello, World!</BlockQuote>
+      <InlineCode>Hello, World!</InlineCode>
+      <Large>Hello, World!</Large>
+      <Small>Hello, World!</Small>
+      <Muted>Hello, World!</Muted>
+      <P>Hello, World!</P>
     </>
   );
 }
