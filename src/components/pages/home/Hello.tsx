a6d93a180604fd1e3f433e85754d88a1f060ff0c
yasainet <takumi.mizoguchi@gmail.com>
2024-11-24 22:48:02 +0900

    feat(UploadButton): アップロードボタンコンポーネントを追加 修正(Hello): 不要なインポートと要素を削除し、コードを簡素化

--- src/components/pages/home/Hello.tsx
+++ src/components/pages/home/Hello.tsx
@@ -1,19 +1,9 @@
-import { BlockQuote, H1, H2, H3, H4, InlineCode, Large, Lead, Muted, P, Small } from '@/components/ui/typography';
+import { H1 } from '@/components/ui/typography';
 
 export default function Hello() {
   return (
     <>
       <H1>Hello, World!</H1>
-      <H2>Hello, World!</H2>
-      <H3>Hello, World!</H3>
-      <H4>Hello, World!</H4>
-      <Lead>Hello, World!</Lead>
-      <BlockQuote>Hello, World!</BlockQuote>
-      <InlineCode>Hello, World!</InlineCode>
-      <Large>Hello, World!</Large>
-      <Small>Hello, World!</Small>
-      <Muted>Hello, World!</Muted>
-      <P>Hello, World!</P>
     </>
   );
 }
